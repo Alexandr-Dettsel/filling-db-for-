beb3f12df484073d4c0806ce1d02a09e378f9538
avdettsel <avdettsel@edu.hse.ru>
2025-10-13 00:54:27 +0300

    checkbox solution

--- main.py
+++ main.py
@@ -1,34 +1,69 @@
+import io
+import os
+
+from PIL import Image
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
+from twocaptcha import TwoCaptcha
 
 import time
-import random
+
+from selenium.webdriver.support.wait import WebDriverWait
 
 BASE_URL = "https://www.chipdip.ru"
 
-def random_sleep(min_sec=1.5, max_sec=3.5):
-    delay = random.uniform(min_sec, max_sec)
-    print(f"Пауза {delay:.2f} сек")
-    time.sleep(delay)
+
+def solver_simple_captcha(driver):
+    try:
+        iframe = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "iframe[data-testid='checkbox-iframe']"))
+        )
+        driver.switch_to.frame(iframe)
+
+        time.sleep(1)
+
+        checkbox = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "js-button"))
+        )
+
+        driver.execute_script("""
+            var event = new MouseEvent('click', {
+                view: window,
+                bubbles: true,
+                cancelable: true
+            });
+            arguments[0].dispatchEvent(event);
+        """, checkbox)
+        driver.switch_to.default_content()
+
+        return True
+
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        driver.switch_to.default_content()
+        return False
+
 
 def get_product_features(driver, url):
     driver.get(url)
-    random_sleep()
+
     try:
         name_product = driver.find_element(By.TAG_NAME, 'h1')
         print(f"\nТовар: {name_product.text}")
     except NoSuchElementException:
         while True:
             print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA CAPCHA AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+            solver_simple_captcha(driver)
             time.sleep(20)
             try:
                 if driver.find_element(By.TAG_NAME, 'h1') is not None:
                     break
             except NoSuchElementException:
-                print("реши капчу уже")
+                pass
 
         get_product_features(driver, url)
 
@@ -44,7 +79,6 @@ def get_product_features(driver, url):
                     });
                     arguments[0].dispatchEvent(event);
                 """, button)
-        time.sleep(1)
 
     except NoSuchElementException:
         pass
@@ -55,11 +89,8 @@ def get_product_features(driver, url):
     for name, value in zip(names, values):
         print(f"{name.text}: {value.text}")
 
-    random_sleep(1, 2)
-
 def parsing_page(driver, url):
     driver.get(url)
-    random_sleep()
 
     while True:
         product_elements = driver.find_elements(By.CSS_SELECTOR, "tr.with-hover a.link")
@@ -72,7 +103,6 @@ def parsing_page(driver, url):
             next_button = driver.find_element(By.CSS_SELECTOR, "a.link.pager__page.pager__control.pager__next")
             next_href = next_button.get_attribute('href')
             driver.get(next_href)
-            random_sleep(3, 6)
         except NoSuchElementException:
             print("Достигнута последняя страница каталога.")
             break
@@ -85,7 +115,6 @@ def main():
 
     catalog_url = f"{BASE_URL}/catalog/electronic-components"
     driver.get(catalog_url)
-    time.sleep(3)
 
     category_elements = driver.find_elements(By.CSS_SELECTOR, "li.catalog__item a.link")
     category_links = [el.get_attribute('href') for el in category_elements]
